24c912935db0d9818a201276c5ebcef67f21621b
LorDTunE <37368884+LorDTunE@users.noreply.github.com>
2019-01-30 13:21:57 +0100

    timeToDie

--- EgenSpel/Fly.cs
+++ EgenSpel/Fly.cs
@@ -11,12 +11,16 @@ namespace EgenSpel
     // Code for the points in the game
     class Fly : PhysicalObject
     {
+        double timeToDie;
         public Fly(Texture2D texture, float X, float Y, GameTime gameTime) : base(texture, X, Y, 0, 2f)
         {
+            timeToDie = gameTime.TotalGameTime.TotalMilliseconds + 3000;
         }
 
         public void Update(GameTime gameTime)
         {
+            if (timeToDie < gameTime.TotalGameTime.TotalMilliseconds)
+                isAlive = false;
         }
     }
 }
